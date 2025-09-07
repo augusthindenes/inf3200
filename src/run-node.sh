7b8c20a00f0a668f79fb9555046777bce80588a7
August Hindenes <august.hindenes@nhn.no>
2025-09-07 19:34:40 +0200

    pass node/hostname to webserver

--- src/run-node.sh
+++ src/run-node.sh
@@ -4,29 +4,25 @@ set -Eeuo pipefail
 GITHUB_TARBALL_URL="${GITHUB_TARBALL_URL:-https://github.com/augusthindenes/inf3200/releases/latest/download/webserver-x86_64-unknown-linux-gnu.tar.gz}"
 
 usage() {
-    echo "Usage: $0 <port>"
+    echo "Usage: $0 <node> <port>"
     echo "Set GITHUB_TARBALL_URL to the .tar.gz for the script to work env variable or in the script"
     exit 1
 }
 
-# --- Check that port argument exists ---
-if [ $# -ne 1 ]; then
+# --- Check that we have exactly 2 arguments ---
+if [ $# -ne 2 ]; then
     usage
 fi
 
-PORT="$1"
+
+NODE="$1"
+PORT="$2"
 
 # --- check that port is a number between 1 and 65535 ---
 if ! [[ "$PORT" =~ ^[0-9]+$ ]] || (( PORT < 1 || PORT > 65535 )); then
     echo "Error: Port must be a number between 1 and 65535" >&2
     exit 2
 fi
-    
-
-# --- Ensure we have the dependencies needed to fetch hostname ---
-for cmd in curl tar hostname; do
-    command -v "$cmd" >/dev/null || { echo "Missing dependency: $cmd" >&2; exit 3; }
-done
 
 # --- workspace & cleanup ---
 WORKDIR="$(mktemp -d)"
@@ -38,22 +34,19 @@ trap cleanup EXIT
 # --- download tarball ---
 echo "Downloading: $GITHUB_TARBALL_URL"
 curl -fsSL "$GITHUB_TARBALL_URL" -o "$TARBALL"
-[[ -s "$TARBALL" ]] || { echo "Error: Download failed or empty file." >&2; exit 4; }
+[[ -s "$TARBALL" ]] || { echo "Error: Download failed or empty file." >&2; exit 3; }
 
 # --- ensure binary exists ---
 mapfile -t entries < <(tar -tzf "$TARBALL")
 BIN="${entries[0]}"
-[[ -n "$BIN" ]] || { echo "Error: Could not find binary in tarball." >&2; exit 5; }
+[[ -n "$BIN" ]] || { echo "Error: Could not find binary in tarball." >&2; exit 4; }
 
 # -- extract and make binary executable ---
 tar -xzf "$TARBALL" -C "$WORKDIR"
 BINPATH="$WORKDIR/$BIN"
 chmod +x "$BINPATH"
 
-# --- get hostname and run the binary ---
-HOST="$(hostname -f 2>/dev/null || hostname)"
+nohup "$BINPATH" "$NODE" "$PORT" &> /dev/null &
 
-nohup "$BINPATH" "$HOST" "$PORT" &> /dev/null &
-
-echo "Exiting node ${HOST}"
+echo "Exiting node ${NODE}"
 exit 0
--- src/run.sh
+++ src/run.sh
@@ -21,12 +21,6 @@ if ! [[ "$NODES" =~ ^[0-9]+$ ]] || (( NODES < 1 || NODES > 100 )); then
     echo "Error: Nodes must be a number between 1 and 100" >&2
     exit 2
 fi
-    
-
-# --- Ensure we have the dependencies needed to fetch hostname ---
-for cmd in curl tar hostname; do
-    command -v "$cmd" >/dev/null || { echo "Missing dependency: $cmd" >&2; exit 3; }
-done
 
 # --- workspace & cleanup ---
 WORKDIR="$(mktemp -d)"
@@ -38,22 +32,19 @@ trap cleanup EXIT
 # --- download tarball ---
 echo "Downloading: $GITHUB_TARBALL_URL"
 curl -fsSL "$GITHUB_TARBALL_URL" -o "$TARBALL"
-[[ -s "$TARBALL" ]] || { echo "Error: Download failed or empty file." >&2; exit 4; }
+[[ -s "$TARBALL" ]] || { echo "Error: Download failed or empty file." >&2; exit 3; }
 
 # --- ensure binary exists ---
 mapfile -t entries < <(tar -tzf "$TARBALL")
 BIN="${entries[0]}"
-[[ -n "$BIN" ]] || { echo "Error: Could not find binary in tarball." >&2; exit 5; }
+[[ -n "$BIN" ]] || { echo "Error: Could not find binary in tarball." >&2; exit 4; }
 
 # -- extract and make binary executable ---
 tar -xzf "$TARBALL" -C "$WORKDIR"
 BINPATH="$WORKDIR/$BIN"
 chmod +x "$BINPATH"
 
-# --- get hostname and run the binary ---
-HOST="$(hostname -f 2>/dev/null || hostname)"
+nohup "$BINPATH" "$NODES" &> /dev/null &
 
-nohup "$BINPATH" "$HOST" "$NODES" &> /dev/null &
-
-echo "Exiting node ${HOST}"
+echo "Exiting node ${NODES}"
 exit 0
